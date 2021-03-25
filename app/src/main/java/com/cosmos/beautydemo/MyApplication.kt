6aed7bc68a79cb60cd964ae39d6fca9a9733761a
df.fqyiran <fan.qiang_2499@immomo.com>
2021-03-25 12:29:33 +0800

    修改:删除无用代码

--- app/src/main/java/com/cosmos/beautydemo/MyApplication.kt
+++ app/src/main/java/com/cosmos/beautydemo/MyApplication.kt
@@ -2,7 +2,6 @@ package com.cosmos.beautydemo
 
 import android.app.Application
 import android.content.Context
-import com.mm.rifle.Rifle
 
 class MyApplication : Application() {
     companion object {
@@ -11,6 +10,5 @@ class MyApplication : Application() {
     override fun onCreate() {
         context = this
         super.onCreate()
-        Rifle.init(this, "6b38bc8e6afdbd040b8f6386b65c0aac", false);
     }
 }
