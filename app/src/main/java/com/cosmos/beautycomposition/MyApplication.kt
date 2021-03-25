668df8227569f50d16805c74f4bd93e3dc561a95
df.fqyiran <fan.qiang_2499@immomo.com>
2021-03-25 12:35:38 +0800

    修改:删除无用代码

--- app/src/main/java/com/cosmos/beautycomposition/MyApplication.kt
+++ app/src/main/java/com/cosmos/beautycomposition/MyApplication.kt
@@ -3,7 +3,6 @@ package com.cosmos.beautycomposition
 import android.app.Application
 import android.content.Context
 import com.cosmos.beautymmdemo.BeautyApplication
-import com.mm.rifle.Rifle
 import com.qiniu.pili.droid.streaming.demo.StreamingApplication
 import com.tencent.liteav.demo.livepusher.camerapush.TxApplication
 import io.agora.api.example.AgoraApplication
@@ -21,7 +20,6 @@ class MyApplication : Application() {
     override fun onCreate() {
         context = this
         super.onCreate()
-        Rifle.init(this, cosmosAppId, false)
 
         BeautyApplication.cosmosAppId = cosmosAppId
         TxApplication.onCreate(this, cosmosAppId)
